5477a660a6dd29c9060de2836787aebb01d5bd58
Jhoifha Winola <jhoifhawinola@gmail.com>
2026-09-01 20:52:35 +0700

    credit habis

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -170,8 +170,8 @@ export default function Projects() {
                       className="group inline-flex items-center gap-2 text-xs font-medium transition-colors hover:opacity-80 sm:text-sm"
                       style={{ color: "var(--putih)" }}
                     >
-                      View Case Study
-                      <ArrowUpRight className="h-4 w-4 transition-transform group-hover:-translate-y-0.5 group-hover:translate-x-0.5" />
+                      {/* View Case Study
+                      <ArrowUpRight className="h-4 w-4 transition-transform group-hover:-translate-y-0.5 group-hover:translate-x-0.5" /> */}
                     </a>
                   </div>
                 </div>
